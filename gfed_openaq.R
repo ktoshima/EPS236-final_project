ff9d931c2c1db2df9517666fafb886981576bb06
ktoshima <24969845+ktoshima@users.noreply.github.com>
2020-12-05 03:40:24 +0900

    bootstrap on CCF lag

--- gfed_openaq.R
+++ gfed_openaq.R
@@ -1,10 +1,13 @@
 ## ACF analysis on the GFED and OpenAQ dataset
 setwd("~/projects/EPS236-final_project/")
+library(boot)
 
 # GFED vs PM2.5
 
 file_lst = list.files('./openAQ_term_pm25/')
 
+acf_lag_pm25 = rep(NA, length(file_lst))
+i = 1
 for (filename in file_lst) {
   df_gfed = read.csv(file.path('gfed_fire_term', filename))
   df_aq = read.csv(file.path('openAQ_term_pm25', filename))
@@ -12,14 +15,19 @@ for (filename in file_lst) {
   emission = df_gfed$emission[1:length(ozone)]
   png(paste("gfed-pm25/", filename, ".png", sep = ""), width = 300, height = 300)
   ccf_values = ccf(emission, ozone, lag.max=30, plot=TRUE, main="PM2.5 & emission", na.action=na.pass)
-  dev.off()                
-  print(ccf_values$lag[abs(ccf_values$acf) >= 0.4])
+  dev.off()            
+  acf_lag[i] = ccf_values$lag[which.max(abs(ccf_values$acf))]
+  i = i+1
 }
+# bootstrap on the lag of largest ACF 
+boot(acf_lag, statistic=function(x,i) mean(x[i]), length(acf_lag))
 
 # GFED vs Ozone
 
 file_lst = list.files('./openAQ_term_ozone/')
 
+acf_lag_ozone = rep(NA, length(file_lst))
+i = 1
 for (filename in file_lst) {
   df_gfed = read.csv(file.path('gfed_fire_term', filename))
   df_aq = read.csv(file.path('openAQ_term_ozone', filename))
@@ -28,8 +36,11 @@ for (filename in file_lst) {
   png(paste("gfed-ozone/", filename, ".png", sep = ""), width = 300, height = 300)
   ccf_values = ccf(emission, ozone, lag.max=30, plot=TRUE, main="ozone & emission", na.action=na.pass)
   dev.off()                
-  print(ccf_values$lag[abs(ccf_values$acf) >= 0.4])
+  acf_lag[i] = ccf_values$lag[which.max(abs(ccf_values$acf))]
+  i = i+1
 }
+# bootstrap on the lag of largest ACF
+boot(acf_lag, statistic=function(x,i) mean(x[i]), length(acf_lag))
 
 # GFED vs Denver-Aurora Ozone
 
@@ -37,7 +48,8 @@ df_aq_denver = read.csv('denver_o3.csv')
 ozone = df_aq_denver$value
 
 file_lst = list.files('./gfed_august/')
-
+acf_lag_denver = rep(NA, length(file_lst))
+i = 1
 for (filename in file_lst) {
   df_gfed = read.csv(file.path('gfed_august', filename))
   emission = df_gfed$emission
@@ -47,7 +59,6 @@ for (filename in file_lst) {
     png(paste("gfed-ozone-denver/", filename, ".png", sep = ""), width = 300, height = 300)
     ccf_values = ccf(emission, ozone, lag.max=30, plot=TRUE, main="ozone & emission", na.action=na.pass)
     dev.off()
-    print(ccf_values$lag[abs(ccf_values$acf) >= 0.4])
   }
 }
 
