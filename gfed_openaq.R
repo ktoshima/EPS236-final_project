8c72e908629f52705bddadd08a640a6b4e212653
ktoshima <24969845+ktoshima@users.noreply.github.com>
2020-12-04 13:04:14 +0900

    add comments

--- gfed_openaq.R
+++ gfed_openaq.R
@@ -1,19 +1,37 @@
 ## ACF analysis on the GFED and OpenAQ dataset
 setwd("~/projects/EPS236-final_project/")
-df_gfed = read.csv('./gfed_fire_term/-112.5--110.0_42.5-40.0_248-272.csv')
-df_aq = read.csv('./openAQ_term_pm25/-112.5--110.0_42.5-40.0_248-272.csv')
 
-file_lst = list.files('./gfed_fire_term/')
+# GFED vs PM2.5
+
+file_lst = list.files('./openAQ_term_pm25/')
 
 for (filename in file_lst) {
   df_gfed = read.csv(file.path('gfed_fire_term', filename))
   df_aq = read.csv(file.path('openAQ_term_pm25', filename))
   ozone = df_aq$value
   emission = df_gfed$emission[1:length(ozone)]
-  png(paste("pm25", filename, ".png", sep = ""), width = 300, height = 300)
-  ccf(emission, ozone, lag.max=30, plot=TRUE, main="ozone & emission", na.action=na.pass)
+  png(paste("gfed-pm25/", filename, ".png", sep = ""), width = 300, height = 300)
+  ccf_values = ccf(emission, ozone, lag.max=30, plot=TRUE, main="PM2.5 & emission", na.action=na.pass)
   dev.off()                
+  print(ccf_values$lag[abs(ccf_values$acf) >= 0.4])
 }
+
+# GFED vs Ozone
+
+file_lst = list.files('./openAQ_term_ozone/')
+
+for (filename in file_lst) {
+  df_gfed = read.csv(file.path('gfed_fire_term', filename))
+  df_aq = read.csv(file.path('openAQ_term_ozone', filename))
+  ozone = df_aq$value
+  emission = df_gfed$emission[1:length(ozone)]
+  png(paste("gfed-ozone/", filename, ".png", sep = ""), width = 300, height = 300)
+  ccf_values = ccf(emission, ozone, lag.max=30, plot=TRUE, main="ozone & emission", na.action=na.pass)
+  dev.off()                
+  print(ccf_values$lag[abs(ccf_values$acf) >= 0.4])
+}
+
+# GFED vs Denver-Aurora Ozone
 
 df_aq_denver = read.csv('denver_o3.csv')
 ozone = df_aq_denver$value
@@ -26,9 +44,10 @@ for (filename in file_lst) {
   if (all(emission == 0)) {
     next
   } else {
-    png(paste("ccf-pm25", filename, ".png", sep = ""), width = 300, height = 300)
-    ccf(emission, ozone, lag.max=30, plot=TRUE, main="ozone & emission", na.action=na.pass)
+    png(paste("gfed-ozone-denver/", filename, ".png", sep = ""), width = 300, height = 300)
+    ccf_values = ccf(emission, ozone, lag.max=30, plot=TRUE, main="ozone & emission", na.action=na.pass)
     dev.off()
+    print(ccf_values$lag[abs(ccf_values$acf) >= 0.4])
   }
 }
 
